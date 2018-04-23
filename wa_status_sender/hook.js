5b20f40e31f52db851ae87b1acb1ec8148272397
Srikanth <kanth.shanigaram@gmail.com>
2018-04-23 20:18:37 +0530

    Hook update status messages

--- wa_status_sender/hook.js
+++ wa_status_sender/hook.js
@@ -1,18 +1,25 @@
 document.getElementById("saveBtn").addEventListener("click", function(){
 	var urlError = document.getElementById('urlError');
+	var urlSuccess = document.getElementById('urlSuccess');
 	var url = document.getElementById("url").value;
+	urlError.style.display = 'none';
+	urlSuccess.style.display = 'none';
     var re = /^(http[s]?:\/\/){0,1}(www\.){0,1}[a-zA-Z0-9\.\-]+\.[a-zA-Z]{2,5}[\.]{0,1}/;
-	/*if (!re.test(url)) { 
+	if (!re.test(url)) { 
     	urlError.style.display = 'block';
     	return false;
-	}else{*/
+	}else{
 		urlError.style.display = 'none';
 		chrome.storage.local.set({'url': url}, function() {
           console.log('Value is set to ' + url);
         });
-	//}
+		urlSuccess.style.display = 'block';
+	}
 });
 chrome.storage.local.get(['url'], function(result) {
           console.log('Value currently is ' + result.url);
+          if(result.url!=undefined)
           document.getElementById("url").value=result.url;
 });
+var urlSuccess = document.getElementById('urlSuccess');
+urlSuccess.style.display = 'none';
